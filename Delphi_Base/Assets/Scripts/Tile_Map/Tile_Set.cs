f482ce3d1dea452cd1e070519788c2cc370bcb1b
Delphine <80095054+delphine-birch@users.noreply.github.com>
2022-05-20 10:50:16 +0100

    Update Tile_Set.cs
    Added extra tile set functionality - specified default slots, empty tiles and tile set saving.

--- Delphi_Base/Assets/Scripts/Tile_Map/Tile_Set.cs
+++ Delphi_Base/Assets/Scripts/Tile_Map/Tile_Set.cs
@@ -3,16 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+#0: Default Flat
+#1: Default Edge
+#2: Default Corner
+#3: Default Corridor
+#4: Default End
+#5: Default Full
+#6: Default Wall
+#7: Default Door
+#8: Default Stair
+#9: Default Ladder
+
 [System.Serializable]
 public class Tile_Set : ScriptableObject
 {
     public Dictionary<int, Tile_Template> tiles;
     public Tile_Set() { tiles = new Dictionary<int, Tile_Template>(); }
-    public Tile_Set(Dictionary<int, Tile_Template> t) { tiles = t; }
-    public Tile_Set(List<Tile_Template> t) {
+    public Tile_Set(List<Tile_Template> t, Tile_Template e) {
         tiles = new Dictionary<int, Tile_Template>();
         int count = 0;
         foreach (Tile_Template tt in t) { tiles[count] = tt; count++; }
+        tiles[-1] = e;
+    }
+    public Tile_Set(List<Tile_Set_Struct> t) {
+        tiles = new Dictionary<int, Tile_Template>();
+        foreach (Tile_Set_Struct tss in t) {
+            tiles[t.i] = t.tt;
+        }
     }
     public Tile_Template Get_Tile(int n) { return tiles[n]; }
+    public void Save(string s) {
+        List<Tile_Set_Struct> tiles0 = new List<Tile_Set_Struct>();
+        foreach (KeyValuePair<int, Tile_Template> kv in tiles) {
+            tiles0.Add(new Tile_Set_Struct { tt = kv.Value, i = kv.Key });
+        }
+        File_Manager.Save(tiles0, s, "Tile_Set");
+    }
+}
+
+[System.Serializable]
+public struct Tile_Set_Struct {
+    public Tile_Template tt;
+    public int i;
 }
