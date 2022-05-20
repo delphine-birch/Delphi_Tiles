508ca1736e244d07747c78f076d0031e4611b925
Delphine <80095054+delphine-birch@users.noreply.github.com>
2022-05-20 13:17:55 +0100

    Update Delphi_Tiles.cs

--- Delphi_Base/Assets/Scripts/Delphi_Tiles.cs
+++ Delphi_Base/Assets/Scripts/Delphi_Tiles.cs
@@ -1,1 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
 
+public class Delphi_Tiles : MonoBehaviour {
+  public Map_Manager map;
+  public Entity_Manager entities;
+  public Render_Manager renderer;
+  
+  public Tile_Map_Data map_data;
+  public Initial_Entity_Data entity_data;
+  
+  void Awake() {
+    map.Initialise(map_data);
+    entities.Initialise(entity_data);
+    renderer.Initialise(this);
+  }
+}
