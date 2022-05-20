c56b2d79f5b3c812eab640863179b9d450eb4fc9
Delphine <80095054+delphine-birch@users.noreply.github.com>
2022-05-20 13:37:37 +0100

    Update Entity_Manager.cs

--- Delphi_Base/Assets/Scripts/Entities/Entity_Manager.cs
+++ Delphi_Base/Assets/Scripts/Entities/Entity_Manager.cs
@@ -1,1 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class Entity_Manager : MonoBehaviour
+{
+    public List<DT_Entity> entities;
+    
+    public void Initialise(Initial_Entity_Data data) {
+      entities = dt.entities;
+    }
+}
+
+public struct Initial_Entity_Data
+{
+  public List<DT_Entity> entities;
+}
