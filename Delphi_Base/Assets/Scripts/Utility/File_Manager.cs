463d65b8d3f388699cca1137803171e5228176cc
Delphine <80095054+delphine-birch@users.noreply.github.com>
2022-05-20 11:08:59 +0100

    Update File_Manager.cs
    Basic File Management

--- Delphi_Base/Assets/Scripts/Utility/File_Manager.cs
+++ Delphi_Base/Assets/Scripts/Utility/File_Manager.cs
@@ -1,1 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
 
+public static class File_Manager
+{
+    public static void Save_List<T>(List<T> l, string filename, string location) {
+        AssetDatabase.CreateAsset(l, "Assets/" + location + "/" + filename + ".asset");
+    }
+    
+    public static void Save_Tile(Tile_Template tt, string filename) {
+        AssetDatabase.CreateAsset(tt, "Assets/Tiles/" + filename + ".asset");
+    }
+    
+    public static Tile_Template Load_Tile(string filename) {
+        return (Tile_Template)AssetDatabase.LoadAssetAtPath("Assets/Tiles/" + filename + ".asset", typeof(Tile_Template));
+    }
+}
